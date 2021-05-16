78fbe0198b86f45f16226c80ad20e40c18835758
Anupam Ashish Minz <anupamminz300am@gmail.com>
2021-05-16 09:26:16 +0530

    fetch data for profile

--- src/routes/profile/profile.js
+++ src/routes/profile/profile.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './profile.module.css';
 
 const Profile = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(()=>{
+    fetch("/api/logged_in_user")
+      .then(data => data.json())
+      .then(data => setUser(data));
+  }, []);
 
   return (
     <div className={styles.baseContainer}>
-      this is the profile
+      Welcome, {user}
       <form action="/logout" method="POST">
         <button type="submit" className={styles.logoutButton}>logout</button>
       </form>
